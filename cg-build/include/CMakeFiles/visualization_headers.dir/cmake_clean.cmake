8af7ac2254b4ad8270365d60ab555fee6781b9aa
Daniel Galper <daniel.galper@gmai.com>
2013-05-01 20:01:14 +0400

    correct

--- cg-build/include/CMakeFiles/visualization_headers.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/visualization_headers"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/visualization_headers.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- cg-build/tests/CMakeFiles/cg_test_headers.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/cg_test_headers"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/cg_test_headers.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
